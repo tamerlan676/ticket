ef8dc78071690ff7bc10b8745c5bb3db5c759446
tamerlan676 <tamerlan676@gmail.com>
2021-07-05 22:30:47 +0300

    start

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">
+    <h1>test</h1>
+    <h2>test</h2>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang='scss'>
+.main{
+  background: red;
+  h1{
+    color: green
+  }
+}
+</style>
