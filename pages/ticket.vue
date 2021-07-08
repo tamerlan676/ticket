1bd497bef224153bc4f9af95033f53698f09f395
tamerlan676 <tamerlan676@gmail.com>
2021-07-08 21:01:50 +0300

    add scheme

--- pages/ticket.vue
+++ pages/ticket.vue
@@ -7,25 +7,31 @@
             .namefield {{ name }}
         .options
          h2 Счастливый билет
-         .price 990 
-         form
-            .step-title 1. Выберите город своей мечты
-            .towns-wrapper
-                .item(v-for="(item, key) in towns" :key="key")
-                    input(class="radio" type="radio" :id="item.title" :value="item.title" v-model="selected" @click="showCity(key, item.img)")
-                    img(:src="item.flag")
-                    label(:for="item.title") {{ item.title }}
-            .step-title 2. Введите данные пассажира
-            .field-block
-                input(class="input" type="text" @input="changeName"  placeholder="Введите имя")
-                input(class="input" type="text" @input="changeDate"  placeholder="Введите дату")
-                input(class="input" type="text"  placeholder="Введите модель телефона")
-            .step-title 3. Посмотрите на чехол, а затем оставьте свои данные и мы отправим его вам!
-            .field-block
-                input(class="input" type="text"  placeholder="Имя")
-                input(class="input" type="text"  placeholder="Номер телефона")
-                input(class="input" type="text"  placeholder="Полный адрес")
-            button(type="submit") Заказать чехол
+         .price 990 ₽
+         .step-title 1. Выберите город своей мечты
+         .towns-wrapper
+            .item(v-for="(item, key) in towns" :key="key")
+                input(class="radio" type="radio" :id="item.title" :value="item.title" v-model="selected" @click="showCity(key, item.img)")
+                img(:src="item.flag")
+                label(:for="item.title") {{ item.title }}
+         .step-title 2. Введите данные пассажира
+         .field-block
+            input(class="input" type="text" @input="changeName"  placeholder="Введите имя")
+            input(class="input" type="text" @input="changeDate"  placeholder="Введите дату")
+            input(class="input" type="text"  placeholder="Введите модель телефона")
+         .step-title 3. Посмотрите на чехол, а затем оставьте свои данные и мы отправим его вам!
+         .field-block
+            input(class="input" type="text"  placeholder="Имя")
+            input(class="input" type="text"  placeholder="Номер телефона")
+            input(class="input" type="text"  placeholder="Полный адрес")
+         button(type="submit") Заказать чехол
+    .ticket-scheme
+        .image
+            img(src="~/assets/images/scheme.jpg")
+        .info
+            h3 Стильная защита
+            p Наш новый противоударный футляр версии 3 обеспечивает максимальную защиту, функциональность и стиль. Изготовлен из полужесткого ТПУ толщиной 2,0 мм с нашими уникальными ударными краями TP-Shock.
+                    
 </template>
 
 <script>
@@ -168,7 +174,7 @@ export default {
         }
         .price{
             font-weight: 500;
-            font-size: 17px;
+            font-size: 19px;
             color: #118B58;
             margin-bottom: 24px;
         }
@@ -230,6 +236,28 @@ export default {
             background: red;
         }
     }
+  }
+  .ticket-scheme{
+      display: grid;
+      background: white;
+      @media (min-width: 992px) {
+          grid-template-columns: 1fr 1fr;
+          grid-column-gap: 32px;
+      }
+       @media (min-width: 1200px) {
+           width: 1120px;
+           margin: 0 auto;
+           align-items: center;
+       }
+      .image{
+          img{
+              max-width: 100%;
+          }
+      }
+      .info{
+          padding: 16px;
+      }
+
   }
 }
 </style>
